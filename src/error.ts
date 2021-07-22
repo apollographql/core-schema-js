689c2a875c401d42d00e90646771da0e38eff5b3
Ashi Krishnan <queerviolet@github.com>
2021-07-21 20:35:07 -0400

    cleanup error module

--- src/error.ts
+++ src/error.ts
@@ -1,41 +1,7 @@
 import { ASTNode, GraphQLError, printError, Source } from 'graphql'
 import { Maybe } from 'graphql/jsutils/Maybe'
 
-export interface GraphQLErrorProps {
-  code: string,
-  message: string,
-  nodes?: Maybe<ReadonlyArray<ASTNode> | ASTNode>,
-  source?: Maybe<Source>,
-  positions?: Maybe<ReadonlyArray<number>>,
-  path?: Maybe<ReadonlyArray<string | number>>,
-  originalError?: Maybe<Error>,
-  extensions?: Maybe<{ [key: string]: any }>,
-  causes?: Error[]
-}
-
-export function toGraphQLError<P extends GraphQLErrorProps>(props: P | Error): GraphQLError {  
-  if (props instanceof Error) return props as GraphQLError
-  const localProps: any = {...props}
-  if (localProps.cause)
-    localProps.cause = toGraphQLError(localProps.cause)
-  if (Array.isArray(localProps.causes))
-    localProps.causes = localProps.causes.map(toGraphQLError)
-  const error = new GraphQLError(
-    localProps.message,
-    localProps.nodes,
-    localProps.source,
-    localProps.positions,
-    localProps.path,
-    localProps.originalError,
-    localProps.extensions
-  )
-  ;(error as any).code = localProps.code
-  
-  return error
-}
-
-
-type Props = {
+export type Props = {
   message: string,
   nodes?: Maybe<ReadonlyArray<ASTNode> | ASTNode>,
   source?: Maybe<Source>,
