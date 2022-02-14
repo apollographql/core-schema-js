280b9949d1d2868770d07888aa6a9975b90f58d4
Trevor Scheer <trevor.scheer@gmail.com>
2022-02-14 15:33:39 -0800

    Fix the bug - don't recursively call toString()

--- src/error.ts
+++ src/error.ts
@@ -1,73 +1,111 @@
-import { ASTNode, GraphQLError, printError, Source } from 'graphql'
-import { Maybe } from 'graphql/jsutils/Maybe'
+import {
+  ASTNode,
+  GraphQLError,
+  printLocation,
+  printSourceLocation,
+  Source,
+} from "graphql";
+import { Maybe } from "graphql/jsutils/Maybe";
 
 export type Props = {
-  message: string,
-  nodes?: Maybe<ReadonlyArray<ASTNode> | ASTNode>,
-  source?: Maybe<Source>,
-  positions?: Maybe<ReadonlyArray<number>>,
-  path?: Maybe<ReadonlyArray<string | number>>,
-  originalError?: Maybe<Error>,
-  extensions?: Maybe<{ [key: string]: any }>,
-  causes?: Error[]
-}
+  message: string;
+  nodes?: Maybe<ReadonlyArray<ASTNode> | ASTNode>;
+  source?: Maybe<Source>;
+  positions?: Maybe<ReadonlyArray<number>>;
+  path?: Maybe<ReadonlyArray<string | number>>;
+  originalError?: Maybe<Error>;
+  extensions?: Maybe<{ [key: string]: any }>;
+  causes?: Error[];
+};
 
 export class GraphQLErrorExt<C extends string> extends GraphQLError {
-  static readonly BASE_PROPS = new Set('nodes source positions path originalError extensions'.split(' '))
+  static readonly BASE_PROPS = new Set(
+    "nodes source positions path originalError extensions".split(" ")
+  );
 
   readonly name: string;
 
   constructor(public readonly code: C, message: string, props?: Props) {
-    super(message,
+    super(
+      message,
       props?.nodes,
       props?.source,
       props?.positions,
       props?.path,
       props?.originalError,
       props?.extensions
-    )
-    if (props) for (const prop in props) {
-      if (!GraphQLErrorExt.BASE_PROPS.has(prop)) {
-        (this as any)[prop] = (props as any)[prop]
+    );
+    if (props)
+      for (const prop in props) {
+        if (!GraphQLErrorExt.BASE_PROPS.has(prop)) {
+          (this as any)[prop] = (props as any)[prop];
+        }
       }
-    }
 
     this.name = code;
   }
 
-  throw(): never { throw this }
-  toString() {
-    let output = `[${this.code}] ${printError(this as any)}`
-    const causes = (this as any).causes
+  throw(): never {
+    throw this;
+  }
+  toString(): string {
+    let output = `[${this.code}] ${this.printOriginalError(this)}`;
+    const causes = (this as any).causes;
     if (causes && causes.length) {
-      output += '\ncaused by:'
+      output += "\ncaused by:";
       for (const cause of (this as any).causes || []) {
-        if (!cause) continue
-        output += '\n\n  - '
-        output += cause.toString().split('\n').join('\n    ')
+        if (!cause) continue;
+        output += "\n\n  - ";
+        output += cause.toString().split("\n").join("\n    ");
       }
     }
 
-    return output
+    return output;
+  }
+
+  // The previous implementation of `printError`, now `GraphQLError.toString()`
+  printOriginalError(err: GraphQLError) {
+    let output = err.message;
+
+    if (err.nodes) {
+      for (const node of err.nodes) {
+        if (node.loc) {
+          output += "\n\n" + printLocation(node.loc);
+        }
+      }
+    } else if (err.source && err.locations) {
+      for (const location of err.locations) {
+        output += "\n\n" + printSourceLocation(err.source, location);
+      }
+    }
+
+    return output;
   }
 }
 
 /**
  * Return a GraphQLError with a code and arbitrary set of properties.
- * 
+ *
  * This mainly helps deal with the very long list of parameters that GraphQLError's constructor
  * can take. It also ensures that all errors have a code, and provides a return typing that
  * facilitates extracting the provided props based on the code, as TypeScript will consider a union of
  * these errors to be a tagged union.
- * 
- * @param code 
- * @param props 
- * @returns 
+ *
+ * @param code
+ * @param props
+ * @returns
  */
-export function err<C extends string, P extends Props>(code: C, props: P | string): GraphQLErrorExt<C> & P {
-  const message = typeof props === 'string' ? props : props.message  
-  const error = new GraphQLErrorExt(code, message, typeof props === 'string' ? undefined : props)
-  return error as any
+export function err<C extends string, P extends Props>(
+  code: C,
+  props: P | string
+): GraphQLErrorExt<C> & P {
+  const message = typeof props === "string" ? props : props.message;
+  const error = new GraphQLErrorExt(
+    code,
+    message,
+    typeof props === "string" ? undefined : props
+  );
+  return error as any;
 }
 
-export default err
+export default err;
