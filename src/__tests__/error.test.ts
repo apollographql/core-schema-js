23e0255b6fe5d997940f845a86a24442ebd35fae
Trevor Scheer <trevor.scheer@gmail.com>
2022-02-16 10:19:20 -0800

    fix: Don't call `GraphQLError.toString()` recursively (#36)

--- src/__tests__/error.test.ts
+++ src/__tests__/error.test.ts
@@ -1,8 +1,21 @@
 import { ErrCheckFailed } from "../core";
+import { GraphQLErrorExt } from "../error";
 
 describe("GraphQLErrorExt", () => {
   it("correctly self-assigns its name property", () => {
     const error = ErrCheckFailed([]);
     expect(error.name).toEqual("CheckFailed");
+  });
+
+  it("calling `toString` doesn't throw an error", () => {
+    const error = new GraphQLErrorExt("CheckFailed", "Check failed");
+    expect(() => error.toString()).not.toThrow();
+  });
+
+  it("calling `toString` prints the error", () => {
+    const error = new GraphQLErrorExt("CheckFailed", "Check failed");
+    expect(error.toString()).toMatchInlineSnapshot(
+      `"[CheckFailed] Check failed"`
+    );
   });
 });
